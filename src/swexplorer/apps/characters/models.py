2d8c8b670ac669680e307825dd945dbc04c9d8db
Markus Zapke-Gründemann <markus@keimlink.de>
2020-10-18 23:23:14 +0200

    Add Dataset model to store meta data for CSV datasets

--- src/swexplorer/apps/characters/models.py
+++ src/swexplorer/apps/characters/models.py
@@ -1,3 +1,17 @@
+import os
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Dataset(models.Model):
+    path = models.FilePathField(
+        path=os.path.join(settings.MEDIA_ROOT, "characters"), match=r".*\.csv$", unique=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return os.path.basename(self.path)
